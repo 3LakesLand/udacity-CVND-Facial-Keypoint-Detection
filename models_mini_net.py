16179d65d1e52cb4683b19e37877dee9c9f2e87c
Klaus Schlichting <kschlichting@gmx.ch>
2018-10-10 23:03:49 +0200

    Add the used Model

---
+++ models_mini_net.py
@@ -0,0 +1,78 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # self.conv1 = nn.Conv2d(1, 32, 5)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+		
+		# Covolutional Layers
+        self.conv_1 = nn.Conv2d(1, 16, 5) 
+        self.conv_2 = nn.Conv2d(16, 64, 3) 
+        self.conv_3 = nn.Conv2d(64, 256, 3) 
+        self.conv_4 = nn.Conv2d(256, 512, 1) # statt 3
+
+		# Maxpooling Layer	(for all)	
+        self.pool = nn.MaxPool2d(2, 2)
+		
+		# Dropout (for all)	
+        self.drop = nn.Dropout(0.6)
+		
+		# Fully Connected Layers (fc)
+        #self.fc_1 = nn.Linear(in_features = 73728, out_features = 5000)  
+        #self.fc_2 = nn.Linear(in_features = 5000, out_features =  5000)  
+        #self.fc_3 = nn.Linear(in_features =  5000, out_features =   136)    #68 keypoints with x and y coordinate => out_features: 136
+		
+        #self.fc_1 = nn.Linear(in_features = 86528, out_features = 1000)  
+        #self.fc_2 = nn.Linear(in_features = 1000, out_features =  1000)  
+        #self.fc_3 = nn.Linear(in_features = 1000, out_features =  1000)  
+        #self.fc_4 = nn.Linear(in_features =  1000, out_features =   136)    #68 keypoints with x and y coordinate => out_features: 136
+	
+        self.fc_1 = nn.Linear(in_features = 173056, out_features = 1000)  
+        self.fc_2 = nn.Linear(in_features = 1000, out_features =  1000)  
+        self.fc_3 = nn.Linear(in_features =  1000, out_features =   136)    #68 keypoints with x and y coordinate => out_features: 136
+		
+		# Epoch: 3, Batch: 260, Avg. Loss: 0.0007566223330795765 (Kernelsize 5,3,3,1 and Dropout(0.6); 173056 in_features)
+		# Avg. Loss: from 0.0040 - 0.0020
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        
+		# Covolutional Layer
+        x = self.pool(F.relu(self.conv_1(x)))
+        x = self.pool(F.relu(self.conv_2(x)))
+        x = self.pool(F.relu(self.conv_3(x)))
+
+		# Flatten the layer
+        x = x.view(x.size(0), -1)
+		
+        # print("in_features size: ", x.size(1))
+
+        x = self.drop(F.relu(self.fc_1(x)))
+        x = self.drop(F.relu(self.fc_2(x)))
+        x = self.fc_3(x)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
